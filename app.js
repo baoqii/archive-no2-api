15378a46bff32a2d7bdb95aa069d40099c321f36
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-08-17 22:13:15 +0800

    Add mongodb connection

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 const app = express();
 
