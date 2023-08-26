c18b76e3e9d363297fb532f93bb79e0dca59ece4
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-08-26 10:38:44 +0800

    Change author references

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const { DateTime } = require("luxon");
 
 const PostSchema = new Schema(
   {
-    author: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, default: Date.now, required: true },
