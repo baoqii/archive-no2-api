4ad8d95dfef52d6ec645bff08314cc4710ab1e1e
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-15 11:20:20 +0800

    Configure cors

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",").map(
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
